27704b78b03633531858a8a1427199333431f973
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-26 11:46:28 -0600

    php

--- web/home.php
+++ web/home.php
@@ -6,6 +6,14 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1" />
   	<link rel="stylesheet" type="text/css" href="home.css">
   	<script type="text/javascript" src="hello.js"></script>
+  	<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Ethan Picklesimer Home</title>
 	</head>
 
