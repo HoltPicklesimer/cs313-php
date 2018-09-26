a12e68e46647bf0d5b431c4640715b2a648c53c4
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-26 11:54:06 -0600

    php

--- web/home.php
+++ web/home.php
@@ -2,31 +2,29 @@
 <html lang="en">
 
   <head>
+    <title>Ethan Picklesimer Home</title>
   	<meta charset="utf-8" />
   	<meta name="viewport" content="width=device-width, initial-scale=1" />
-  	<link rel="stylesheet" type="text/css" href="home.css">
-  	<script type="text/javascript" src="hello.js"></script>
-  	<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  	<!-- Latest compiled and minified CSS for Bootstrap -->
+		<link rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+		</script>
 
-		<!-- Latest compiled JavaScript -->
+		<!-- Latest compiled JavaScript for Bootstrap -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>Ethan Picklesimer Home</title>
 	</head>
 
 	<body>
 
-		<h1>Welcome to my Home Page</h1>
-		<div id="div1" onclick="myFunction()">Hello World!</div>
-		<p id="p1">
-			This is to help me <span style="text:bold"><i>freshen up</i></span>
-			on my <span style="border:4px solid black"><b>HTML</b></span>
-			skills.
-		</p>
-		<div id="div2">Heroku uses Git's commands in a similar way.</div>
+	<div class="container">
+		<h1>Ethan Picklesimer Home</h1>
+		<h2>Welcome to my CS 313 Home Page</h2>
+		<p>This is my first time using Bootstrap</p>
+	</div>
 	</body>
 
 </html>
