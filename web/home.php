73b4b9e56170b961ef7f29f1ec41179904699970
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-26 11:41:11 -0600

    Added Home Page

---
+++ web/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+  	<meta charset="utf-8" />
+  	<meta name="viewport" content="width=device-width, initial-scale=1" />
+  	<link rel="stylesheet" type="text/css" href="home.css">
+  	<script type="text/javascript" src="hello.js"></script>
+    <title>Ethan Picklesimer Home</title>
+	</head>
+
+	<body>
+
+		<h1>Welcome to my Home Page</h1>
+		<div id="div1" onclick="myFunction()">Hello World!</div>
+		<p id="p1">
+			This is to help me <span style="text:bold"><i>freshen up</i></span>
+			on my <span style="border:4px solid black"><b>HTML</b></span>
+			skills.
+		</p>
+		<div id="div2">Heroku uses Git's commands in a similar way.</div>
+	</body>
+
+</html>
