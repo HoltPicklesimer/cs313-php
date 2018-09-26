de46e1af212eb1edc41ba5de51ba26c433527290
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-26 15:50:25 -0600

    php

--- web/home.php
+++ web/home.php
@@ -28,7 +28,7 @@
 		    <div class="col-sm-4 links">.col-sm-4</div>
 		</div> -->
 
-		<div class="container-fluid bg-1 text-center">
+		<div class="container-fluid bg-3 text-center">
 			<h3>Ethan Picklesimer Home</h3>
 			<h3>Welcome to my CS 313 Home Page</h3>
 		</div>
