68b869cce3975f299005e5fe9dafb65df6f2ca8f
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-26 18:22:46 -0600

    php

--- web/heading.php
+++ web/heading.php
@@ -2,7 +2,7 @@
 	echo "<div class='links' id='top'>
 					<a href='home.php' class='t1'>HOME</a>
 					<a href='assigns.php' class='t1'>ASSIGNMENTS</a>
-					<a href='#about' class='t1'>ABOUT</a>
-					<a href='#skills' class='t1'>SKILLS</a>
+					<a href='home.php#about' class='t1'>ABOUT</a>
+					<a href='home.php#skills' class='t1'>SKILLS</a>
 				</div>";
 ?>
