6510ecccd250fa5ee055cbf3120910453fac67b2
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-26 19:14:03 -0600

    php

--- web/home.php
+++ web/home.php
@@ -54,7 +54,7 @@
 			<p>Familiar with: C#, Python, SQL</p>
 		</div>
 
-		<div class="bg-1">
+		<div class="container-fluid bg-1">
 			<form class="container-fluid bg-3 text-center" onreset="clearErrors()">
 				<p>Enter "hello" into username and "world"
 				into password to see the secret message.</p>
