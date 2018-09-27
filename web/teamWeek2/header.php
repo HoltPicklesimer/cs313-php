a88f19a3f77e362a98f3fe28bc675d477abe03d3
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-27 16:21:51 -0600

    Team Activity

--- web/teamWeek2/header.php
+++ web/teamWeek2/header.php
@@ -5,6 +5,7 @@
 
 <div class='topNav container'>
 	<h1>Piano Movers</h1>
+	<?php echo $page; ?>
 	<a class='<?php if ($page == "siteHome.php") echo active; ?>' href='siteHome.php'>HOME</a>
 	<a class='<?php if ($page == "login.php") echo active; ?>' href='login.php'>LOGIN</a>
 	<a class='<?php if ($page == "aboutUs.php") echo active; ?>' href='aboutUs.php'>ABOUT</a>
