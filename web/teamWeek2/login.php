95944c8440d0fc5afa64aadad25b67b304a94a10
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-27 15:57:46 -0600

    Team Activity

---
+++ web/teamWeek2/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Piano Movers Login</title>
+  	<meta charset="utf-8" />
+  	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  	<!-- Latest compiled and minified CSS for Bootstrap -->
+		<link rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<link rel="stylesheet"
+		href="team2.css"/>
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+		</script>
+
+		<!-- Latest compiled JavaScript for Bootstrap -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+		</script>
+	</head>
+
+	<body>
+
+		<div class="container">
+			<h1>LOGIN</h1>
+		</div>
+
+		<?php include 'header.php'; ?>
+
+		<div class="container">
+			<p>Welcome, you are not logged in.</p>
+		</div>
+
+		<button type="button">Login as Administrator</button>
+		<button type="button">Login as Tester</button>
+
+
+	</body>
+
+</html>
