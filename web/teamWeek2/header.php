983de3a4bfafb81b717a610bf7ca2a0b266bfe44
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-09-27 16:45:03 -0600

    Team Activity

--- web/teamWeek2/header.php
+++ web/teamWeek2/header.php
@@ -1,12 +1,12 @@
 <?php
 
-	$page = $_SERVER['PHP_SELF', PATHINFOFILENAME];
+	$page = $_SERVER['PHP_SELF'];
 ?>
 
 <div class='topNav container'>
 	<h1>Piano Movers</h1>
 	<?php echo $page; ?>
-	<a class='<?php if ($page == "/teamWeek2/siteHome.php") echo "active"; ?>' href='siteHome.php'>HOME</a>
-	<a class='<?php if ($page == "/teamWeek2/login.php") echo "active"; ?>' href='login.php'>LOGIN</a>
-	<a class='<?php if ($page == "/teamWeek2/aboutUs.php") echo "active"; ?>' href='aboutUs.php'>ABOUT</a>
+	<a class='<?php if ($page == "/teamWeek2/siteHome.php") echo active; ?>' href='siteHome.php'>HOME</a>
+	<a class='<?php if ($page == "/teamWeek2/login.php") echo active; ?>' href='login.php'>LOGIN</a>
+	<a class='<?php if ($page == "/teamWeek2/aboutUs.php") echo active; ?>' href='aboutUs.php'>ABOUT</a>
 </div>
