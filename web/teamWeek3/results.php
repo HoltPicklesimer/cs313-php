a31a81f11ab2ee931ae7ac5e186e2bad1c77ad64
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-02 16:09:43 -0600

    team 3

---
+++ web/teamWeek3/results.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$major = $_POST["major"];
+	$comment = $_POST["comment"];
+
+	echo "Name: $name<br/>
+				mailto: $email<br/>
+				Major: $major<br/>
+				Comments: $comment<br/>";
+?>
