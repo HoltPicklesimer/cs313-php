e9b625634ad553bd250548ea6c6d45fcd0f046b0
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-03 17:19:31 -0600

    Shopping Cart

--- web/ShoppingCart/browseItems.php
+++ web/ShoppingCart/browseItems.php
@@ -106,7 +106,7 @@
 								<input type="hidden" name="name" value="<?php echo $selected->name; ?>" />
 								<input type="hidden" name="price" value="<?php echo $selected->price; ?>" />
 								<input type="button" name="add_to_cart" class="btn btn-info"
-								value="Add to Cart" onclick='browseItems.php.href="?button1=1"'/>
+								value="Add to Cart" onclick='location.href="?button1=1"'/>
 								<?php echo $selected->quantity; ?>
 							</div>
 						</div>
