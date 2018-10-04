3bd32e4dce3c975828087af4da4995e887c1c36d
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-04 16:04:17 -0600

    cookies

---
+++ web/Sessions/load_cookie.php
@@ -0,0 +1,16 @@
+<?php
+$favorite = $_COOKIE["fav-text"];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		echo "Your favorite text was $favorite";
+	?>
+
+</body>
+</html>
