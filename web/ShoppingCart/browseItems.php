a8a067e27a68bee3e9b3fbeccd9f19955d57cc5f
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-03 20:10:21 -0600

    Shopping Cart

--- web/ShoppingCart/browseItems.php
+++ web/ShoppingCart/browseItems.php
@@ -62,6 +62,7 @@
 
 	function fun1()
 	{
+		echo "fun1";
 		$_SESSION["shopping_cart"][0][$_SESSION["index"]]->quantity = $_POST["quantity" . str_replace(' ', '_', $selected->name)];
 	}
 	function fun2()
