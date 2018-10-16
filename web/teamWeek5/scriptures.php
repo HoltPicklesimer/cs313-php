90c6c1f60fed5c5041316cb798ef5c2e0c0304ac
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-16 16:00:31 -0600

    Team Week 5

---
+++ web/teamWeek5/scriptures.php
@@ -0,0 +1,24 @@
+<?php require "dbConnect.php"; ?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>SCripture Resources</title>
+</head>
+<body>
+
+	<h1>Scriptures Resources</h1>
+
+	<?php
+
+	foreach ($db->query('SELECT * FROM Scriptures') as $row)
+	{
+	  echo '<p><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . ' - "' . $row['content'] . '"</p>';
+	}
+
+	?>
+
+</body>
+
+</html>
