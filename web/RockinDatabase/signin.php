2d2d66b86e150b1d44bf0ee3176c3f78224f2e0d
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-17 17:10:35 -0600

    Sign In Screen

--- web/RockinDatabase/signin.php
+++ web/RockinDatabase/signin.php
@@ -33,7 +33,7 @@
 	<h1>Welcome to My Rockin' Database.</h1>
 	<h3>Search songs, song reviews, and artists and find music videos!</h3>
 	<h2>Sign In</h2>
-	<form class="container" align="left">
+	<form class="sm-form" align="left" method="post" action="signin.php">
 		<p>Username: <input type="text" name="user"></p>
 		<p>Password: <input type="text" name="pass"></p>
 		<p><a class="text-info" href="signup.php">Don't Have an Account? Click here to sign up!</a></p>
