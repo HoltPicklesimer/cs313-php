e1972aa32d5024327996d651c7f3652f571a46b6
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-20 13:25:25 -0600

    Header

---
+++ web/RockinDatabase/header.php
@@ -0,0 +1,8 @@
+<div class="container-fluid header">
+
+	<h1>My Rockin' Database</h1>
+
+	<a href="user.php" class="text-white">Home</a>
+	<a href="siginin.php" class="text-white">Log Out</a>
+
+</div>
