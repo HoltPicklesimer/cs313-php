1eaacfb0c9d72c0f322a90cc18c7e4b37dfcf125
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-23 16:36:50 -0600

    Team Activity 6

--- web/teamWeek5/insertion.php
+++ web/teamWeek5/insertion.php
@@ -11,14 +11,19 @@ if ($_POST)
 	$topicId = $_POST["topicId"];
 }
 
-$stmt = $db->prepare("INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content);
-	INSERT INTO ScriptureTopics (scripture_id, topic_id) VALUES ((SELECT id FROM Scriptures WHERE book = :book AND chapter = :chapter AND verse = :verse AND content = :content), :topic_id)");
+
+$stmt = $db->prepare("INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)");
+$stmt2 = $db->prepare("SELECT id FROM Scriptures WHERE book = :book AND chapter = :chapter AND verse = :verse AND content = :content");
+$stmt3 = $db->prepare("INSERT INTO ScriptureTopics (scripture_id, topic_id) VALUES :scripture_id, :topic_id");
 $stmt->bindValue(':book', $book, PDO::PARAM_STR);
 $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
 $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->bindValue(':topic_id', $topicId, PDO::PARAM_INT);
 $stmt->execute();
+$stmt2->execute();
+$scripture_id = $stmt2->fetch(PDO::FETCH_ASSOC)["id"];
+$stmt3->execute();
 
 ?>
 
