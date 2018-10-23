c5468629e7b173aea68e297b9ae2e94706d238c0
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-23 16:43:27 -0600

    Team Activity 6

--- web/teamWeek5/insertion.php
+++ web/teamWeek5/insertion.php
@@ -14,16 +14,32 @@ if ($_POST)
 
 $stmt = $db->prepare("INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)");
 $stmt2 = $db->prepare("SELECT id FROM Scriptures WHERE book = :book AND chapter = :chapter AND verse = :verse AND content = :content");
-$stmt3 = $db->prepare("INSERT INTO ScriptureTopics (scripture_id, topic_id) VALUES :scripture_id, :topic_id");
 $stmt->bindValue(':book', $book, PDO::PARAM_STR);
 $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
 $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-$stmt->bindValue(':topic_id', $topicId, PDO::PARAM_INT);
 $stmt->execute();
+
+$stmt2->bindValue(':book', $book, PDO::PARAM_STR);
+$stmt2->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+$stmt2->bindValue(':verse', $verse, PDO::PARAM_INT);
+$stmt2->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt2->execute();
+
 $scripture_id = $stmt2->fetch(PDO::FETCH_ASSOC)["id"];
-$stmt3->execute();
+
+$stmt3->bindValue(':book', $book, PDO::PARAM_STR);
+$stmt3->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+$stmt3->bindValue(':verse', $verse, PDO::PARAM_INT);
+$stmt3->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt3->bindValue(':scripture_id', $scripture_id, PDO::PARAM_INT);
+
+foreach ($topicId as $topic) {
+	$stmt3 = $db->prepare("INSERT INTO ScriptureTopics (scripture_id, topic_id) VALUES :scripture_id, :topic_id");
+	$stmt3->bindValue(':topic_id', $topic, PDO::PARAM_INT);
+	$stmt3->execute();
+}
+
 
 ?>
 
