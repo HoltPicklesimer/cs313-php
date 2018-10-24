0587eb7ce5dc4ac1394901649fa9945d6861a9d4
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-24 11:37:44 -0600

    artists

--- web/RockinDatabase/artist.php
+++ web/RockinDatabase/artist.php
@@ -265,7 +265,7 @@ foreach ($playlist as $song) {
 
 </div>
 
-<?php } if ($message != "") { echo "<script type='text/javascript'>alert(" . $message . ");</script>"; } ?>
+<?php } if ($message != "") { echo "<script type='text/javascript'>alert('" . $message . "');</script>"; } ?>
 
 </body>
 
