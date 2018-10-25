49535d79d05f2416d9a745009c1fd94d88d1efa6
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-25 16:12:28 -0600

    Group Activity Week 6

--- web/RockinDatabase/review.php
+++ web/RockinDatabase/review.php
@@ -199,11 +199,11 @@ if ($reviewId > 0)
 		<div class="form-group">
 		  <label for="sel1">Select Rating: </label>
 		  <select class="form-control" id="sel1" name="newRating">
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-				<option value="4">4</option>
-				<option value="5">5</option>
+				<option value=1>1</option>
+				<option value=2>2</option>
+				<option value=3>3</option>
+				<option value=4>4</option>
+				<option value=5>5</option>
 		  </select>
 		</div>
 
---
+++ web/groupWeek6/courses.php
@@ -0,0 +1,40 @@
+<?php
+
+require ('dbConnect.php');
+
+$db = get_db();
+
+$query = 'SELECT id, code, name FROM course';
+
+$stmt = $db->prepare($query);
+$stmt->execute();
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses</title>
+</head>
+<body>
+	<h1>Courses</h1>
+
+	<ul>
+
+<?php
+
+foreach ($course as $course) {
+	$id = $course['id'];
+	$name = $course['name'];
+	$code = $course['course'];
+
+	echo "<li><p>$code - $name</p></li>";
+}
+
+?>
+
+	</ul>
+
+</body>
+</html>
