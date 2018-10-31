7f7a02c40b5f6ea4fcce8f29eb2f849fc17df118
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-30 18:26:45 -0600

    login

--- web/RockinDatabase/user.php
+++ web/RockinDatabase/user.php
@@ -6,6 +6,15 @@ require "connectDb.php";
 // Redirect if the user id is not set or invalid
 require("verifyUser.php");
 
+if (!isset($_SESSION))
+	session_start();
+
+// If the user is not logged in, go to the Log-In Screen
+if (!isset($_SESSION["userId"]))
+	header('Location: signin.php');
+if ($_SESSION["userId"] < 1)
+	header('Location: signin.php');
+
 // Get the user id
 $id = $_SESSION["userId"];
 
