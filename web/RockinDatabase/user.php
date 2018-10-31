3c0bad8a7c37e930ff4a39ba64569e471944d4d6
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-10-30 18:10:42 -0600

    log out if not a user

--- web/RockinDatabase/user.php
+++ web/RockinDatabase/user.php
@@ -3,17 +3,8 @@
 // Connect to the Database
 require "connectDb.php";
 
-if (!isset($_SESSION))
-	session_start();
-
-// If the user is not logged in, go to the Log-In Screen
-if (!isset($_SESSION["userId"]))
-	header('Location: signin.php');
-if ($_SESSION["userId"] < 1)
-	header('Location: signin.php');
-
-// Get the user id
-$id = $_SESSION["userId"];
+// Redirect if the user id is not set or invalid
+require("verifyUser.php");
 
 // Delete song if delete button is pressed
 if ($_POST)
